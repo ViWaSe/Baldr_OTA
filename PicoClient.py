8b059e4ef80cc69772b32b94fee601ddf8661087
Baldr.os <126325748+ViWaSe@users.noreply.github.com>
2025-06-27 18:02:56 +0200

    Update PicoClient.py

--- PicoClient.py
+++ PicoClient.py
@@ -4,7 +4,7 @@
 # The incoming orders are processed and executed by order.py and the answer is published to the status-topic
 # Settings stored in config.json
 
-version = '6.2.1'
+version = '6.2.1a'
 
 import utime as time
 from mqtt_handler import MQTTHandler
@@ -63,11 +63,8 @@ def watchdog(
 
         if wd_counter % 2 == 0:
             Log('Watchdog', '[ CHECK ]: Very quiet here. Checking connection...')
-            if not check_status():
-                Log('Watchdog', '[ FAIL ]: No Connection to Wifi. See Wifi.log for details!')
-                return False
             
-            mqtt.publish(f'{mqttClient}/status', 'echo')
+            mqtt.publish(f'{mqttClient}/status', {"msg": "echo", "is_err_msg": False, "origin": "watchdog"})
             mqtt.set_rec(False)
 
             for i in range (timeout_loops):
@@ -78,7 +75,9 @@ def watchdog(
                     break
                 time.sleep_ms(timeout_pause)
             if not state:
-                    Log('Watchdog', '[ WARN  ]: Message wait timeout. Probably connection lost')
+                    Log('Watchdog', '[ FAIL  ]: Message wait timeout. Probably connection lost')
+                    if not check_status():
+                        Log('Watchdog', '[ FAIL  ]: No Connection to Wifi. See Wifi.log for details!')
                     mqtt.reconnect()
     return True
 
