2f8a269479e7d1dce98dcb05840e48e869606366
Baldr.os <126325748+ViWaSe@users.noreply.github.com>
2026-02-13 21:23:10 +0100

    Add files via upload

--- LightControl.py
+++ LightControl.py
@@ -5,13 +5,13 @@
 # NOTE: The "type: ignore" commtents are for the vs-code micropico extension only! The main reason is that the values from the JSON-File are unknown
 # NOTE: For WW/CW LEDs (24V): setting bpp = 3 is required (Byte 0=warm, 1=cold, 2=not used)
 
-version='6.4.0'
+version=[7,2,1]
 
 import utime as time
 from neopixel import NeoPixel
 from machine import Pin
 from json_config_parser import config
-from PicoWifi import Log
+import logger
 
 try:
     from typing import Any
@@ -19,28 +19,76 @@ except ImportError:
     Any = object
 
 class LightControl:
-    def __init__(self, config_file='/params/config.json', status_file='/params/status.json'):
-        self.settings   = config(config_file, layers=2)
+    def __init__(
+            self,
+            use_config_json=True, 
+            config_file='/params/config.json', 
+            status_file='/params/status.json',
+            logging=True,
+            led_pin=15,
+            bpp=3,
+            pixel_pty=12,
+            autostart=True
+            ):
+        
+        """
+        Parameters:
+            use_config_json (bool): True, if a JSON file is used for settings. Must have 2 layers. The json_config_parser is needed.
+            config_file (str): Location of the configuration-file.
+            status_file (str): Location of the status-file, where level and color will be saved.
+            logging (bool): Events are logged in the /log/ directory
+            led_pin (int): Neopixel LED-Pin.
+            bpp (int): Bytes per pixel value. 3=RGB, 4=RGBW
+            pixel_pty (int): The Number of LEDs that are adressed.
+            autostart (bool): Load the last color- and dim- setting from the status.json file.
+
+        Methods:
+        --------
+            static(): sets all pixels at the same time.
+            line(): sets pixels by line-animation
+            set_smooth(): sets pixels by a smooth transition
+            set_dim(): sets a dimmer-level
+            change_autostart(): sets the autostart-state. If true, the pixels will be set to the last known state when power on
+            change_pixel_qty(): changes the pixel-quantity
+            ret_dim(self): returns the actual light level
+        """
+
+        if use_config_json:
+            self.settings   = config(config_file, layers=2)
+            self.led_pin    = self.settings.get('LightControl_settings', 'led_pin')
+            self.bpp        = self.settings.get('LightControl_settings', 'bytes_per_pixel')
+            self.autostart  = self.settings.get('LightControl_settings', 'autostart')
+            self.pixel_qty  = self.settings.get('LightControl_settings', 'led_qty')
+        else:
+            self.led_pin    = led_pin
+            self.bpp        = bpp
+            self.pixel_qty  = pixel_pty
+            self.autostart  = autostart
+
         self.status     = config(status_file, layers=1)
         self.cache      = self.status.get(param='color')
-
-        self.led_pin    = self.settings.get('LightControl_settings', 'led_pin')
-        self.bpp        = self.settings.get('LightControl_settings', 'bytes_per_pixel')
-        self.autostart  = self.settings.get('LightControl_settings', 'autostart')
         self.dim_status = self.status.get(param='dim_status')
 
+        if logging:
+            self.event = logger.Create('LightControl', '/log')
+        else:
+            self.event = logger.DummyLogger()
+
         # Check Pixel-Value
-        pixel_value     = self.settings.get('LightControl_settings', 'led_qty')
-        if pixel_value is None:
+        if self.pixel_qty is None:
             missing_pixel_error = "Missing 'led_gty' in config.json!"
-            Log('LC',f' [ ERROR ]: {missing_pixel_error}')
+            self.event.log('E', f'Initialization failed! - {missing_pixel_error}')
             return missing_pixel_error
         
-        self.pixel: int = int(pixel_value)
+        self.pixel: int = int(self.pixel_qty)
 
         self.level = 0
         self.led = Pin(self.led_pin, Pin.OUT, value=0)
         self.np: Any = NeoPixel(self.led, self.pixel, bpp=self.bpp) # type: ignore # type
+
+        # Set save-timer
+        self.last_change = 0
+        self.need_save = False
 
         if self.autostart:
             self.set_dim(self.dim_status)
@@ -49,18 +97,17 @@ class LightControl:
     def static(
             self, 
             color, 
-            level=1
+            level_255=None
             ):
-        color = list(color)
         while len(color) < 4:
             color.append(0)
+        
+        if level_255 is None:
+            level_255 = int(self.level * 255)
+        dimmed_color = tuple((c * level_255) // 255 for c in color[:4])
+
         for i in range(self.pixel):
-            self.np[i] = ( 
-                int(color[0] * level),
-                int(color[1] * level),
-                int(color[2] * level),
-                int(color[3] * level)
-            )
+            self.np[i] = dimmed_color
         self.np.write()
         self.cache = color
         return color
@@ -74,28 +121,41 @@ class LightControl:
     def set_dim(
             self, 
             target, 
-            speed=1
+            speed=1,
+            steps=2
             ):
+        
+        """
+        Dims the LED-levels in a smooth animation.
+
+        Parameter:
+            target (int): Target-Level. Values from 0-100.
+            speed (int): Pause between the 1%-steps in ms.
+        """
+
         actual = int(self.level * 100)
         target = int(target)
         if actual == target:
             return
 
         if target > actual:
-            self._ramp_up(actual, target, speed)
+            self._ramp_up(actual, target, speed, steps)
         else:
-            self._ramp_down(actual, target, speed)
+            self._ramp_down(actual, target, speed, steps)
 
-        self.status.save_param(param='dim_status', new_value=target)
+        self.last_change = time.time()
+        self.needs_save = True
+        return True
 
     def _ramp_up(
             self, 
             actual, 
             target, 
-            speed
+            speed,
+            steps=2
             ):
         while actual < target:
-            actual += 1
+            actual += steps
             if actual > target:
                 actual = target
             self.level = actual / 100
@@ -106,10 +166,11 @@ class LightControl:
             self, 
             actual, 
             target, 
-            speed
+            speed,
+            steps=2
             ):
         while actual > target:
-            actual -= 1
+            actual -= steps
             if actual < target:
                 actual = target
             self.level = actual / 100
@@ -136,7 +197,7 @@ class LightControl:
                 int(color[3] * light_level)
             )
             self.np.write()
-        except:
+        except IndexError:
             pass
 
     # set color by line animation
@@ -146,8 +207,19 @@ class LightControl:
             speed=5, 
             dir=0, 
             gap=1, 
-            start=0
+            start=0,
             ):
+        
+        """
+        Sets the color by a line-animation.
+
+        Parameter:
+            color (list): Target-color in rgbw-list-format. A list with 4 objects is expected (e.g. [255,0,0,0]).
+            speed (int): Pause between the steps in ms.
+            dir (int): Direction of the line. 1=forward, 2=backwards.
+            gap (int): Gap betwen the leds in the animation. 1=no gap.
+            start (int): start of the animation. 0=start on the first led.
+        """
         
         line = start
         color = list(color)
@@ -176,7 +248,8 @@ class LightControl:
                 line -= gap
                 time.sleep_ms(speed)
         self.cache = color
-        self.status.save_param(param='color', new_value=color)
+        self.last_change = time.time()
+        self.needs_save = True
         return color
     
     # set Color by soft transition
@@ -186,6 +259,15 @@ class LightControl:
             speed=10, 
             steps=50
             ):
+        
+        """
+        Change the color by smooth transition.
+        
+        Parameters:
+            target_color(list): A list with 4 integer objects with the color-value (R,G,B,W | 0...255). At least one value is needed. If one is missing (i.e. W), only the given colors are set (255,255 -> 255,255,0,0).
+            speed(int): Pause between steps in ms. 10 is default.
+            steps(int): Transition steps between pause. 50 is recommended to run a smooth animation.
+            """
         
         current = list(self.cache) # type: ignore
         target = list(target_color)
@@ -203,7 +285,8 @@ class LightControl:
             time.sleep_ms(speed)
         
         self.cache = target
-        self.status.save_param(param='color', new_value=target)
+        self.last_change = time.time()
+        self.needs_save = True
         return target
 
     def change_autostart(self, value):
@@ -211,13 +294,34 @@ class LightControl:
     
     def change_pixel_qty(self, value):
         self.status.save_param(param='led_qty', new_value=value)
+    
+    def get_info(self):
+        return {"version": version, "led_qty": self.pixel_qty, "led_pin": self.led_pin, "color": self.cache, "light_level": self.dim_status}
+    
+    def check_save(self, force=False):
+        if force or (self.need_save and (time.time() - self.last_change) > 5):
+            self.status.save_param(param='color', new_value=self.cache)
+            self.status.save_param(param='dim_status', new_value=int(self.level * 100))
+            self.need_save = False
 
-    def ret_dim(self):
-        return self.status.get(param='dim_status')
+# Auto-initialize LEDs
+LC = LightControl()
 
-LC = LightControl()     # For standalone usage
-
-# Examples:
-# LC = LightControl()
-# LC.set_dim(80, 10) --> Dim to level 80% with 10ms pause between levels
-# LC.line([200,0,0]) --> Set color rgb(255,0,0) by line animation
+"""
+def set_smooth(self, target_color, speed=10, steps=50):
+    current = list(self.cache)
+    target = list(target_color)
+    while len(target) < 4: target.append(0)
+    
+    # Vorbereiten der Differenzen spart Rechenzeit in der Schleife
+    diffs = [target[i] - current[i] for i in range(4)]
+    
+    for step in range(1, steps + 1):
+        # Ganzzahl-Arithmetik für Geschwindigkeit
+        intermediate = [
+            int(current[i] + (diffs[i] * step // steps))
+            for i in range(4)
+        ]
+        self.static(intermediate, self.level)
+        time.sleep_ms(speed)
+"""
---
+++ mqtt_Client.py
@@ -0,0 +1,143 @@
+# MQTT Client Module
+# New Version with separate MQTT-Handler
+# There are 3 topics used, one for incoming order, one for configuration and one for the status from pico. config and status are for publishing message
+# The incoming orders are processed and executed by order.py and the answer is published to the status-topic
+# Settings stored in config.json
+
+version = [7,1,5]
+
+import utime as time
+from mqtt_handler import MQTTHandler
+from uWifi import Client
+from json_config_parser import config
+from ntp_simple import NTP
+import logger
+import sys
+import gc
+from LightControl import LC
+
+# Connect to WLAN using PicoWifi-Module
+wlan = Client()
+wlan.connect()
+print('[ INFO ] PicoWifi is connected!')
+ntp = NTP(
+    use_json_config=True,
+    time_setting_file='/params/time_setting.json'
+)
+ok, msg = ntp.boot()
+print(f'[ INFO ] {msg}')
+
+led_onboard = Client.get_led()
+
+# load settings from the config file
+settings        = config('/params/config.json')
+mqttClient      = settings.get('MQTT-config', 'Client')
+mqttBroker      = settings.get('MQTT-config', 'Broker')
+mqttPort        = settings.get('MQTT-config', 'Port')
+mqttUser        = settings.get('MQTT-config', 'User')
+mqttPW          = settings.get('MQTT-config', 'PW')
+publish_in_Json = settings.get('MQTT-config', 'publish_in_json')
+
+# Set the LED-Timer depending on the platform (pico or not)
+is_pico = sys.platform == 'rp2'
+if is_pico:
+    led_timer=600
+else:
+    led_timer=400
+
+# Create empty variables for watchdog and for the led_toggle-function
+ledCount    = 0
+last_msg    = time.time()
+wd_counter  = 0
+watchdog_last_chk = 0
+
+mqtt = MQTTHandler(
+    client_id=mqttClient,
+    broker=mqttBroker,
+    user=mqttUser,
+    password=mqttPW,
+    pinjson=publish_in_Json # type: ignore
+)
+
+event = logger.Create('Client', '/log')
+wd_event = logger.Create('Watchdog', '/log', 1024)
+
+# Watchdog-function to check the connection to the broker
+def watchdog(
+        watch_time=60, 
+        cooldown=5,
+        timeout_loops=5,
+        timeout_pause=500
+        ):
+    global last_msg, wd_counter, watchdog_last_chk
+
+    pico_time = time.time()
+    if watchdog_last_chk and pico_time - watchdog_last_chk < cooldown:
+        return True 
+
+    if pico_time - last_msg > watch_time:
+        wd_counter += 1
+        wd_event.log('I', f'Counter: {wd_counter} | RTC-Time={pico_time} | Last msg={last_msg}')
+        
+        last_msg = pico_time 
+        watchdog_last_chk = pico_time
+
+        if wd_counter % 2 == 0:
+            wd_event.log('I', f'Very quiet here. Checking connection...')
+            
+            # Publish the echo-message to the /status-topic and set received to false
+            mqtt.publish(f'{mqttClient}/status', {"msg": "echo", "is_err_msg": False, "origin": "watchdog"})
+            mqtt.set_rec(False)
+
+            # check for answer, break if received
+            for i in range (timeout_loops):
+                mqtt.check_msg()
+                state = mqtt.get_rec()
+                if state:
+                    wd_event.log('I', 'Connection still up!')
+                    break
+                time.sleep_ms(timeout_pause)
+            
+            if not state:
+                wd_event.log('E', 'Message wait timeout. Probably connection lost.')
+                mqtt.reconnect()
+    return True
+
+# Function for Onboard-LED as ok indicator and check connection
+def led_toggle(onTime=led_timer):
+    global ledCount
+    ledCount +=1
+    time.sleep_ms(1)
+    if ledCount >= onTime:
+        led_onboard.off()
+        time.sleep_ms(100)
+        led_onboard.on()
+        ledCount = 0
+    
+# Main - just call go() to start the Loop
+def go():
+    while True:
+        if not mqtt.connect():
+            time.sleep(5)
+            continue
+        try:
+            mqtt.subscribe(f'{mqttClient}/order')
+        except Exception as e:
+            event.log ('E', f'Failed to subscribe after successfull connect: {e}')
+        
+        try:
+            while True:
+                led_toggle()
+                mqtt.check_msg()
+                watchdog()
+                LC.check_save()
+        
+        except Exception as e:
+            event.log('E', f'MQTT connection lost! - {e}')
+            try:
+                gc.collect()
+                mqtt.reconnect()
+            except Exception as e:
+                event.log('E', f'Failed to reconnect! [Error: {e}] Reboot...')
+                import machine
+                machine.reset()    
